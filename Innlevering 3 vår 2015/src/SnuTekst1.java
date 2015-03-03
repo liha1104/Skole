0f545977cb1f267e9db5fcdd69c05c625489c5d9
VictorLindbäck <victorlindback2@yahoo.no>
2015-03-03 18:50:04 +0100

    Snutekst v0.2

--- Innlevering 3 vår 2015/src/SnuTekst1.java
+++ Innlevering 3 vår 2015/src/SnuTekst1.java
@@ -4,17 +4,24 @@ public class SnuTekst1 {
 
 	private static Scanner in = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("Kan du være så snill å skrive inn en setning som skal snues?");
-		String userInput = in.nextLine();
+	private static int antall = 0;
 
-		baklengs(userInput);
+	public static void main(String[] args) {
+		System.out
+				.println("Kan du være så snill å skrive inn en setning som skal snues?");
+		String tekst = in.nextLine();
+
+		baklengs(tekst);
 	}
 
 	public static void baklengs(String tekst) {
 		if (tekst.length() > 0) {
 			System.out.print(tekst.charAt(tekst.length() - 1));
-			baklengs(tekst.substring(0, tekst.length()-1));
+			antall++;
+			baklengs(tekst.substring(0, tekst.length() - 1));
+		} else {
+			System.out.println("Det er totalt" + antall + "i tekststrengen.");
+			antall = 0;
 		}
 	}
 }
